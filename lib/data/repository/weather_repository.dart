662c7de072792f9d11040ec5cfd2ac7e3f17d650
Mahmoud <mahmoudabohebil@gmail.com>
2023-08-09 16:48:27 +0300

    Setup parsedJson method in WeatherRepo class to get data from api as WeatherModel

--- lib/data/repository/weather_repository.dart
+++ lib/data/repository/weather_repository.dart
@@ -1,4 +1,14 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class WeatherRepo {}
+import '../model/weather_model.dart';
+
+class WeatherRepo {
+  WeatherModel parsedJson(final response) {
+    final jsonDecoded = json.decode(response);
+
+    final jsonWeather = jsonDecoded["main"];
+
+    return WeatherModel.fromJson(jsonWeather);
+  }
+}
