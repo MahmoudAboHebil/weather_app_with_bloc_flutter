aee0e7fc293bd34510c0f8dd3e0468f7b2f08948
Mahmoud <mahmoudabohebil@gmail.com>
2023-08-09 16:47:44 +0300

    Setup getWeather method in WeatherServices class to get data from api

--- lib/data/web_services/weather_services.dart
+++ lib/data/web_services/weather_services.dart
@@ -1,9 +1,12 @@
 import 'package:http/http.dart' as http;
 
+const apiKey = "3e186f0092055e392c60e2dd6a2c987d";
+const openWeatherMapURL = "https://api.openweathermap.org/data/2.5/weather";
+
 class WeatherServices {
   Future<String> getWeather(String city) async {
-    final result = await http.Client().get(Uri.https(
-        "https://api.openweathermap.org/data/2.5/weather?q=$city&APPID=3e186f0092055e392c60e2dd6a2c987d"));
+    final result = await http.Client().get(
+        Uri.https("$openWeatherMapURL?q=$city&appid=$apiKey&units=metric"));
 
     if (result.statusCode != 200) throw Exception();
     return result.body;
